25d0fcb39a29a75e6bf2c5bb668bcc3215df12f6
David Gutman <dagutman@gmail.com>
2024-03-18 12:29:02 -0400

    deidHelper fix

---
+++ docs/app/components/metaDataUpload_panel.py
@@ -0,0 +1,28 @@
+from dash import dcc, html
+import dash_bootstrap_components as dbc
+
+
+metadata_upload_layout = dbc.Container(
+    [
+        html.Div(
+            "Please drag and drop a CSV file containing metadata linked to the slides of interest below"
+        ),
+        dcc.Upload(
+            id="upload-data",
+            children=html.Div(["Drag and Drop or ", html.A("Select Files")]),
+            style={
+                "width": "100%",
+                "height": "60px",
+                "lineHeight": "60px",
+                "borderWidth": "1px",
+                "borderStyle": "dashed",
+                "borderRadius": "5px",
+                "textAlign": "center",
+                "margin": "10px",
+            },
+            # Allow multiple files to be uploaded
+            multiple=False,
+        ),
+        html.Div(id="output-data-upload"),
+    ]
+)
